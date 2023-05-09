9f0176020454a9af6cc6fe96ff80522ba197c9ea
Alexis Rebhuhn <44133522+Alexisrebhuhn@users.noreply.github.com>
2023-05-09 16:21:51 +0200

    Add files via upload

---
+++ ProjetMadeInValDeLoire_APP/ProjetMadeInValDeLoire/Connexion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetMadeInValDeLoire
+{
+    public partial class Connexion : Form
+    {
+        public Connexion()
+        {
+            InitializeComponent();
+        }
+
+        private void inscrire_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "Inscription réussie";
+            lblMessage.ForeColor = Color.Green;
+        }
+
+        private void btnConnecter_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "Connexion réussie";
+            lblMessage.ForeColor = Color.Green;
+        }
+    }
+}
