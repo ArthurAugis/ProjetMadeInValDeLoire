a21b09ed761e0d8ed47549b6ffbe25f1442828cf
RainbowYoshi <92651106+ArthurAugis@users.noreply.github.com>
2023-05-09 14:28:38 +0200

    Add files via upload

---
+++ ProjetMadeInValDeLoire/Scoreboard.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ProjetMadeInValDeLoire
+{
+    partial class Scoreboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAccueil = new System.Windows.Forms.Button();
+            this.lblTitre = new System.Windows.Forms.Label();
+            this.txtTop1 = new System.Windows.Forms.Label();
+            this.txtTop2 = new System.Windows.Forms.Label();
+            this.txtTop3 = new System.Windows.Forms.Label();
+            this.txtTop6 = new System.Windows.Forms.Label();
+            this.txtTop5 = new System.Windows.Forms.Label();
+            this.txtTop4 = new System.Windows.Forms.Label();
+            this.txtTop9 = new System.Windows.Forms.Label();
+            this.txtTop8 = new System.Windows.Forms.Label();
+            this.txtTop7 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.txtTop10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAccueil
+            // 
+            this.btnAccueil.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAccueil.Location = new System.Drawing.Point(328, 410);
+            this.btnAccueil.Name = "btnAccueil";
+            this.btnAccueil.Size = new System.Drawing.Size(144, 28);
+            this.btnAccueil.TabIndex = 7;
+            this.btnAccueil.Text = "Revenir à l\'accueil";
+            this.btnAccueil.UseVisualStyleBackColor = true;
+            // 
+            // lblTitre
+            // 
+            this.lblTitre.AutoSize = true;
+            this.lblTitre.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitre.Location = new System.Drawing.Point(298, 9);
+            this.lblTitre.Name = "lblTitre";
+            this.lblTitre.Size = new System.Drawing.Size(226, 29);
+            this.lblTitre.TabIndex = 8;
+            this.lblTitre.Text = "Tableau des scores";
+            // 
+            // txtTop1
+            // 
+            this.txtTop1.AutoSize = true;
+            this.txtTop1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTop1.Location = new System.Drawing.Point(12, 56);
+            this.txtTop1.Name = "txtTop1";
+            this.txtTop1.Size = new System.Drawing.Size(64, 20);
+            this.txtTop1.TabIndex = 9;
+            this.txtTop1.Text = "1 - Nom";
+            // 
+            // txtTop2
+            // 
+            this.txtTop2.AutoSize = true;
+            this.txtTop2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTop2.Location = new System.Drawing.Point(12, 87);
+            this.txtTop2.Name = "txtTop2";
+            this.txtTop2.Size = new System.Drawing.Size(64, 20);
+            this.txtTop2.TabIndex = 10;
+            this.txtTop2.Text = "2 - Nom";
+            // 
+            // txtTop3
+            // 
+            this.txtTop3.AutoSize = true;
+            this.txtTop3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTop3.Location = new System.Drawing.Point(12, 116);
+            this.txtTop3.Name = "txtTop3";
+            this.txtTop3.Size = new System.Drawing.Size(64, 20);
+            this.txtTop3.TabIndex = 11;
+            this.txtTop3.Text = "3 - Nom";
+            // 
+            // txtTop6
+            // 
+            this.txtTop6.AutoSize = true;
+            this.txtTop6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTop6.Location = new System.Drawing.Point(12, 206);
+            this.txtTop6.Name = "txtTop6";
+            this.txtTop6.Size = new System.Drawing.Size(64, 20);
+            this.txtTop6.TabIndex = 14;
+            this.txtTop6.Text = "6 - Nom";
+            // 
+            // txtTop5
+            // 
+            this.txtTop5.AutoSize = true;
+            this.txtTop5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTop5.Location = new System.Drawing.Point(12, 177);
+            this.txtTop5.Name = "txtTop5";
+            this.txtTop5.Size = new System.Drawing.Size(64, 20);
+            this.txtTop5.TabIndex = 13;
+            this.txtTop5.Text = "5 - Nom";
+            // 
+            // txtTop4
+            // 
+            this.txtTop4.AutoSize = true;
+            this.txtTop4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTop4.Location = new System.Drawing.Point(12, 146);
+            this.txtTop4.Name = "txtTop4";
+            this.txtTop4.Size = new System.Drawing.Size(64, 20);
+            this.txtTop4.TabIndex = 12;
+            this.txtTop4.Text = "4 - Nom";
+            // 
+            // txtTop9
+            // 
+            this.txtTop9.AutoSize = true;
+            this.txtTop9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTop9.Location = new System.Drawing.Point(12, 295);
+            this.txtTop9.Name = "txtTop9";
+            this.txtTop9.Size = new System.Drawing.Size(64, 20);
+            this.txtTop9.TabIndex = 17;
+            this.txtTop9.Text = "9 - Nom";
+            // 
+            // txtTop8
+            // 
+            this.txtTop8.AutoSize = true;
+            this.txtTop8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTop8.Location = new System.Drawing.Point(12, 266);
+            this.txtTop8.Name = "txtTop8";
+            this.txtTop8.Size = new System.Drawing.Size(64, 20);
+            this.txtTop8.TabIndex = 16;
+            this.txtTop8.Text = "8 - Nom";
+            // 
+            // txtTop7
+            // 
+            this.txtTop7.AutoSize = true;
+            this.txtTop7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTop7.Location = new System.Drawing.Point(12, 235);
+            this.txtTop7.Name = "txtTop7";
+            this.txtTop7.Size = new System.Drawing.Size(64, 20);
+            this.txtTop7.TabIndex = 15;
+            this.txtTop7.Text = "7 - Nom";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(12, 355);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(200, 20);
+            this.lblScore.TabIndex = 19;
+            this.lblScore.Text = "Ton classement est 69ème";
+            // 
+            // txtTop10
+            // 
+            this.txtTop10.AutoSize = true;
+            this.txtTop10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTop10.Location = new System.Drawing.Point(12, 324);
+            this.txtTop10.Name = "txtTop10";
+            this.txtTop10.Size = new System.Drawing.Size(73, 20);
+            this.txtTop10.TabIndex = 18;
+            this.txtTop10.Text = "10 - Nom";
+            // 
+            // Scoreboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.txtTop10);
+            this.Controls.Add(this.txtTop9);
+            this.Controls.Add(this.txtTop8);
+            this.Controls.Add(this.txtTop7);
+            this.Controls.Add(this.txtTop6);
+            this.Controls.Add(this.txtTop5);
+            this.Controls.Add(this.txtTop4);
+            this.Controls.Add(this.txtTop3);
+            this.Controls.Add(this.txtTop2);
+            this.Controls.Add(this.txtTop1);
+            this.Controls.Add(this.lblTitre);
+            this.Controls.Add(this.btnAccueil);
+            this.Name = "Scoreboard";
+            this.Text = "Tableau des scores";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAccueil;
+        private System.Windows.Forms.Label lblTitre;
+        private System.Windows.Forms.Label txtTop1;
+        private System.Windows.Forms.Label txtTop2;
+        private System.Windows.Forms.Label txtTop3;
+        private System.Windows.Forms.Label txtTop6;
+        private System.Windows.Forms.Label txtTop5;
+        private System.Windows.Forms.Label txtTop4;
+        private System.Windows.Forms.Label txtTop9;
+        private System.Windows.Forms.Label txtTop8;
+        private System.Windows.Forms.Label txtTop7;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label txtTop10;
+    }
+}
